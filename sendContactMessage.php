9a2f3ccf933339e187a957228a0bf915a5e775de
bdp6bg <bdp6bg@virginia.edu>
2019-10-28 16:56:57 -0400

    ?

---
+++ sendContactMessage.php
@@ -0,0 +1,39 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+require 'vendor/autoload.php';
+
+
+$name = $_POST["name"];
+$email = $_POST["email"];
+$message = $_POST["message"];
+$url = "Location: /Joystick/index.html";
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;
+    $mail->isSMTP();
+    $mail->Host = 'smtp.gmail.com';
+    $mail->SMTPAuth = true;
+    $mail->Username = 'joystickemail123@gmail.com';
+    $mail->Password = 'Password456!';
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    //Recipients
+    $mail->setFrom($email, $name);
+    $mail->addAddress('joystickemail123@gmail.com');
+    //Content
+    $mail->isHTML(true);
+    $mail->Subject = 'Customer Inquiry';
+    $mail->Body    = nl2br("$name\n\n$email\n\n$message");
+    $mail->send();
+    echo 'Message has been sent';
+} catch (Exception $e) {
+    echo 'Unable to send message. Mailer Error: ', $mail->ErrorInfo;
+}
+
+header("Location: $url");
+
+//exit();
+?>
